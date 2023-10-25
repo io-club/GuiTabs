31332427ea65ca5e826006028917fa0a6c6b9bfe
aixiao0621 <52861243+aixiao0621@users.noreply.github.com>
2023-10-25 23:50:45 +0800

    init

--- src/index.jsx
+++ src/index.jsx
@@ -1,27 +1,88 @@
-import { Route, Router, Routes } from "@solidjs/router"
+import { Route, Router, Routes } from "@solidjs/router";
 import { render } from "solid-js/web";
-import { lazy } from "solid-js";
-const Tabs = lazy(() => import("./tabs"));
-const Todo = lazy(() => import("./todo"));
-// import {Todo} from "./todo";
-// import Tabs from "./tabs";
+import { Button } from "@suid/material";
+import TemporaryDrawer from "./tabs";
+
+import { createSignal } from "solid-js";
 
 function App() {
-
-    return <>
-        <h1 >Guitar Tabs Home</h1>
-        <nav>
-            <a href="/tabs">Tabs</a>
-            <a href="/todo">Todo</a>
-        </nav>
-        <Routes>
-            <Route path="/tabs" component={Tabs} />
-            <Route path="/todo" component={Todo} />
-        </Routes>
+  const [value, setValue] = createSignal("GuiTabs");
+  let allTabs = {
+    Peace: {
+      name: "Peace",
+      url: ["/finger/peace/1.png"],
+    },
+    "like a star": {
+      name: "like a star",
+      url: [
+        "/finger/like_a_star/1.webp",
+        "/finger/like_a_star/2.webp",
+        "/finger/like_a_star/3.webp",
+        "/finger/like_a_star/4.webp",
+        "/finger/like_a_star/5.webp",
+        "/finger/like_a_star/6.webp",
+        "/finger/like_a_star/7.webp",
+      ],
+    },
+  };
+  return (
+    <>
+      <div>
+        <Button variant="text" size="small">
+          <TemporaryDrawer
+            tabs={allTabs}
+            onClick={(title) => {
+              setValue(title);
+            }}
+          ></TemporaryDrawer>
+        </Button>
+        <a
+          style={{
+            "text-align": "right",
+            "text-decoration": "overline",
+            "font-weight": "bold",
+            "margin-left": "10px",
+          }}
+        >
+          {allTabs[value()] ? allTabs[value()].name : "GuiTabs"}
+        </a>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          "flex-direction": "column",
+          "align-items": "center",
+          "justify-content": "center",
+        }}
+      >
+        {allTabs[value()] ? (
+          allTabs[value()].url.map((e, index) => (
+            <>
+              第 {index + 1} 页 👇
+              <img
+                src={`${e}`}
+                style={{
+                  width: `auto`,
+                  height: `auto`,
+                  "max-width": `100%`,
+                  "max-height": `100%`,
+                }}
+              ></img>
+            </>
+          ))
+        ) : (
+          <img src="t.webp"></img>
+        )}
+      </div>
     </>
-};
+  );
+}
 
-render(() =>
+render(
+  () => (
     <Router>
-        <App />
-    </Router>, document.getElementById('root'));
+      <App />
+    </Router>
+  ),
+  document.getElementById("root")
+);
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite'
 import solid from 'vite-plugin-solid'
+import solidPlugin from "vite-plugin-solid";
+import suidPlugin from "@suid/vite-plugin";
 
 export default defineConfig({
-  plugins: [solid()],
+  plugins: [solid(), suidPlugin(), solidPlugin()],
+  build: {
+    target: "esnext",
+  },
 })
